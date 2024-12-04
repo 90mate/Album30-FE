9a28907af3ba653a005c2f1a9a77aedcaf1e8138
20210699 <102691173+20210699@users.noreply.github.com>
2024-12-05 05:22:12 +0900

    [Feature] 초기 설정 (#2)
    * ✨ Feat: 파일 구조 생성
    
    * ✨ Feat: 파일 구조 생성
    
    * 📝 Docs: issue template 생성
    
    * 📝 Docs: bug_report.yml 수정
    
    * 📝 Docs: feature_request.yml 수정
    
    * 📝 Docs: pull_request_template.md 생성

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          💙 우리의 첫 React 프로젝트 💙
         </p>
         <a
           className="App-link"
