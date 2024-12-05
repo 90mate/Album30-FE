6719f2a9e42365cdcd7f4a20115a9194e40803e5
yuchaemin2 <kusuri94@gmail.com>
2024-12-05 16:07:23 +0900

    ✨ Feat: 앨범 상세 페이지 라우터 설정

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import KakaoLogin from './components/Login/Login';
 import App from './App';
+import AlbumDetail from './pages/AlbumDetail/AlbumDetail';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
         <Routes>
             <Route path="/" element={<App />}></Route>
             <Route path="/login" element={<KakaoLogin />}></Route>
+            <Route path="/detail" element={<AlbumDetail />}></Route>
         </Routes>
     </BrowserRouter>
   );
